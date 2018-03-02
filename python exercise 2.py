9e563ead195d810483b59b6e75c1f63cd943507e
rm113866 <35816944+rm113866@users.noreply.github.com>
2018-03-02 13:43:02 -0700

    Delete python exercise 2.py

--- python exercise 2.py
+++
@@ -1,6 +0,0 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> name = input("Enter your name. "); print(name)
-Enter your name. ryan
-ryan
->>> 
